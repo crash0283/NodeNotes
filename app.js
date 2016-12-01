7290c9a15f9af9d166ab79a8460cd2257f8e9c67
Chris <rasch83@gmail.com>
2016-12-01 18:55:29 +0000

    finished lesson 19

--- app.js
+++ app.js
@@ -12,16 +12,34 @@ console.log("Command: ", command);
 console.log('Yargs:', argv);
 
 if (command == 'add') {
-  notes.addNote(argv.title, argv.body);
+  var note = notes.addNote(argv.title, argv.body);
+  
+  if(note) {
+    console.log('Note created!');
+    notes.logNote(note);
+  } else {
+    console.log("Note already added!");
+  }
 } 
 else if(command == 'list'){
   notes.getAll();
 } 
 else if(command == 'read') {
-  notes.readNote(argv.title);
+  var note = notes.readNote(argv.title);
+  
+  if(note) {
+    console.log('Note found!');
+    notes.logNote(note);
+  }
+  else {
+    console.log('Note not found!');
+  }
+  
 }
 else if(command == 'remove') {
-  notes.removeNote(argv.title);
+  var isRemoved = notes.removeNote(argv.title);
+  var message = isRemoved ? 'Note was removed!' : 'Note not found!';
+  console.log(message);
 }
 else {
   console.log('Command not found');
